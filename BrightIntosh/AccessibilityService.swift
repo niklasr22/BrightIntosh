551e21fb08a787ddc62f833a6012fc81a6b08f0f
Niklas Rousset <niklas.rousset@gmail.com>
2023-07-19 12:03:56 +0200

    Adds global hotkey feature.

---
+++ BrightIntosh/AccessibilityService.swift
@@ -0,0 +1,34 @@
+//
+//  AccessibilityService.swift
+//  BrightIntosh
+//
+//  Created by Niklas Rousset on 19.07.23.
+//
+
+import Cocoa
+
+final class AccessibilityService {
+    
+    private static var trusted = false
+    private static var running = false
+    
+    private static func pollIsTrustedProcess(getsTrusted: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            AccessibilityService.trusted = AXIsProcessTrusted()
+            if !AccessibilityService.trusted {
+                AccessibilityService.pollIsTrustedProcess(getsTrusted: getsTrusted)
+            } else {
+                getsTrusted()
+            }
+        }
+    }
+    
+    static func startPollingTrustedProcessState(getsTrusted: @escaping () -> ()) {
+        if AccessibilityService.running {
+            return
+        }
+        AccessibilityService.running = true
+        pollIsTrustedProcess(getsTrusted: getsTrusted)
+    }
+    
+}
