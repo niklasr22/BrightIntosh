c8e9cb309b949134514a002c47087d01f31fab54
Niklas Rousset <niklas.rousset@gmail.com>
2023-07-26 19:08:42 +0200

    test carbon

--- BrightIntosh/AppDelegate.swift
+++ BrightIntosh/AppDelegate.swift
@@ -8,12 +8,29 @@
 import Cocoa
 import SwiftUI
 import ServiceManagement
+import Carbon
 
 struct SwiftUIView: View {
     var body: some View {
         Text("Hello, SwiftUI!")
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+}
+
+extension String {
+  /// This converts string to UInt as a fourCharCode
+  public var fourCharCodeValue: Int {
+    var result: Int = 0
+    if let data = self.data(using: String.Encoding.macOSRoman) {
+      data.withUnsafeBytes({ (rawBytes) in
+        let bytes = rawBytes.bindMemory(to: UInt8.self)
+        for i in 0 ..< data.count {
+          result = result << 8 + Int(bytes[i])
+        }
+      })
+    }
+    return result
+  }
 }
 
 
@@ -148,7 +165,57 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 self.toggleBrightIntosh()
             }
         })
+        
+        
+        
     }
+    
+    static func register() {
+        var hotKeyRef: EventHotKeyRef?
+        let modifierFlags: UInt32 =
+          getCarbonFlagsFromCocoaFlags(cocoaFlags: NSEvent.ModifierFlags.command)
+
+        let keyCode = kVK_ANSI_R
+        var gMyHotKeyID = EventHotKeyID()
+
+        gMyHotKeyID.id = UInt32(keyCode)
+
+        // Not sure what "swat" vs "htk1" do.
+        gMyHotKeyID.signature = OSType("swat".fourCharCodeValue)
+        // gMyHotKeyID.signature = OSType("htk1".fourCharCodeValue)
+
+        var eventType = EventTypeSpec()
+        eventType.eventClass = OSType(kEventClassKeyboard)
+        eventType.eventKind = OSType(kEventHotKeyReleased)
+
+        // Install handler.
+        InstallEventHandler(GetApplicationEventTarget(), {
+          (nextHanlder, theEvent, userData) -> OSStatus in
+          // var hkCom = EventHotKeyID()
+
+          // GetEventParameter(theEvent,
+          //                   EventParamName(kEventParamDirectObject),
+          //                   EventParamType(typeEventHotKeyID),
+          //                   nil,
+          //                   MemoryLayout<EventHotKeyID>.size,
+          //                   nil,
+          //                   &hkCom)
+
+          NSLog("Command + R Released!")
+
+          return noErr
+          /// Check that hkCom in indeed your hotkey ID and handle it.
+        }, 1, &eventType, nil, nil)
+
+        // Register hotkey.
+        let status = RegisterEventHotKey(UInt32(keyCode),
+                                         modifierFlags,
+                                         gMyHotKeyID,
+                                         GetApplicationEventTarget(),
+                                         0,
+                                         &hotKeyRef)
+        assert(status == noErr)
+      }
     
     @objc func toggleBrightIntosh() {
         active.toggle()
