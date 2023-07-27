c7000dfddea96512cfb17e517069c9254cdf7cee
Niklas Rousset <niklas.rousset@gmail.com>
2023-07-27 18:21:52 +0200

    removed unused code

--- BrightIntosh/AppDelegate.swift
+++ BrightIntosh/AppDelegate.swift
@@ -9,22 +9,6 @@ import Cocoa
 import ServiceManagement
 import Carbon
 
-extension String {
-    /// This converts string to UInt as a fourCharCode
-    public var fourCharCodeValue: Int {
-        var result: Int = 0
-        if let data = self.data(using: String.Encoding.macOSRoman) {
-            data.withUnsafeBytes({ (rawBytes) in
-                let bytes = rawBytes.bindMemory(to: UInt8.self)
-                for i in 0 ..< data.count {
-                    result = result << 8 + Int(bytes[i])
-                }
-            })
-        }
-        return result
-    }
-}
-
 
 class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     private var statusItem: NSStatusItem!
