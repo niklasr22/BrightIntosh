a6364c3f84b3fc6583f16668b9e2556a66c2fe42
Niklas Rousset <niklas.rousset@gmail.com>
2023-09-01 22:39:43 +0200

    slider animation

--- slider.js
+++ slider.js
@@ -4,11 +4,37 @@ function initComparisons() {
         compareImages(overlays[i]);
     }
 
+    function animateSlider(slider, overlay) {
+        const overlayMovement = [
+            { width: "0px" },
+            { width: "50%" },
+        ];
+
+        const sliderMovement = [
+            { left: "0" },
+            { left: "50%" },
+        ];
+
+        const sliderTiming = {
+            duration: 600,
+            iterations: 1,
+            easing: "ease-in-out",
+        };
+        let sliderAnim = slider.animate(sliderMovement, sliderTiming);
+        sliderAnim.onfinish = (_) => {
+            slider.style.left = "50%";
+        };
+        let overlayAnim = overlay.animate(overlayMovement, sliderTiming);
+        overlayAnim.onfinish = (_) => {
+            overlay.style.width = "50%";
+        };
+    }
+
     function compareImages(img) {
         var clicked = false;
-        var w = img.offsetWidth;
+        var w = img.parentElement.offsetWidth;
         var h = img.offsetHeight;
-        
+
         img.style.width = (w / 2) + "px";
 
         var slider = document.createElement("div");
@@ -17,7 +43,7 @@ function initComparisons() {
         img.parentElement.insertBefore(slider, img);
 
         slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
-        slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+        slider.style.left = (w / 2) + "px";
 
         slider.addEventListener("mousedown", slideReady);
         window.addEventListener("mouseup", slideFinish);
@@ -54,7 +80,9 @@ function initComparisons() {
 
         function slide(x) {
             img.style.width = x + "px";
-            slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+            slider.style.left = img.offsetWidth + "px";
         }
+
+        animateSlider(slider, img)
     }
 }
