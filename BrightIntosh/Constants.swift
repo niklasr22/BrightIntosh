8fcc99dac5c746e7e49b435cf33c637bc3ba9cbd
Niklas Rousset <niklas.rousset@gmail.com>
2023-09-24 19:03:05 +0200

    update default shortcut for brightness changes

--- BrightIntosh/Constants.swift
+++ BrightIntosh/Constants.swift
@@ -10,8 +10,8 @@ import KeyboardShortcuts
 
 extension KeyboardShortcuts.Name {
     static let toggleBrightIntosh = Self("toggleIncreasedBrightness", default: .init(carbonKeyCode: kVK_ANSI_B, carbonModifiers: (0 | optionKey | cmdKey)))
-    static let decreaseBrightness = Self("decreaseBrightness", default: .init(KeyboardShortcuts.Key.minus, modifiers: [.command, .option]))
-    static let increaseBrightness = Self("increaseBrightness", default: .init(KeyboardShortcuts.Key.equal, modifiers: [.command, .option]))
+    static let decreaseBrightness = Self("decreaseBrightness", default: .init(carbonKeyCode: kVK_ANSI_N, carbonModifiers: (0 | optionKey | cmdKey)))
+    static let increaseBrightness = Self("increaseBrightness", default: .init(carbonKeyCode: kVK_ANSI_M, carbonModifiers: (0 | optionKey | cmdKey)))
 }
 
 let launcherBundleId = "de.brightintosh.launcher" as CFString
