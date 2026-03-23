3dcdd45e8096e7212e9292438863aebe4ebe3e00
Niklas Rousset <niklas.rousset@gmail.com>
2026-03-23 10:19:48 +0100

    adds studio display xdr

--- BrightIntosh/Constants.swift
+++ BrightIntosh/Constants.swift
@@ -29,9 +29,9 @@ let appVersion = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? 
 let supportedDevices = ["MacBookPro18,1", "MacBookPro18,2", "MacBookPro18,3", "MacBookPro18,4", "Mac14,6", "Mac14,10", "Mac14,5", "Mac14,9", "Mac15,7", "Mac15,9", "Mac15,11", "Mac15,6", "Mac15,8", "Mac15,10", "Mac15,3", "Mac16,1", "Mac16,6", "Mac16,8", "Mac16,7", "Mac16,5", "Mac17,2", "Mac17,6", "Mac17,8", "Mac17,7", "Mac17,9"
 ]
 #if DEBUG
-let externalXdrDisplays = ["Pro Display XDR", "C34H89x"]
+let externalXdrDisplays = ["Pro Display XDR", "Studio Display XDR", "C34H89x"]
 #else
-let externalXdrDisplays = ["Pro Display XDR"]
+let externalXdrDisplays = ["Pro Display XDR", "Studio Display XDR"]
 #endif
 let sdr600nitsDevices = ["Mac15,3", "Mac15,6", "Mac15,7", "Mac15,8", "Mac15,9", "Mac15,10", "Mac15,11", "Mac16,1", "Mac16,6", "Mac16,8", "Mac16,7", "Mac16,5", "Mac17,2", "Mac17,6", "Mac17,8", "Mac17,7", "Mac17,9"]
 
