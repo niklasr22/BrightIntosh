36509c2630c1d13829a1a6f8d9b10611ab60c013
Niklas Rousset <niklas.rousset@gmail.com>
2026-04-09 17:47:33 +0200

    rm logs

--- BrightIntosh/AppDelegate.swift
+++ BrightIntosh/AppDelegate.swift
@@ -113,8 +113,6 @@ extension BrightIntoshAppDelegate: NSApplicationDelegate {
         if cliBase() {
             exit(0);
         }
-        print("Thermal info: \(ProcessInfo.processInfo.thermalState)")
-        print("Low power: \(ProcessInfo.processInfo.isLowPowerModeEnabled)")
         
         supportedDevice = isDeviceSupported()
         
